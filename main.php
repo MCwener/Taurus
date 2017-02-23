7a90d10825caf839ba104de442dadf5ec9122a39
Cody Parker <coparker@csumb.edu>
2017-02-23 02:06:11 +0000

    Display randomly generated player hands.

--- main.php
+++ main.php
@@ -1,41 +1,38 @@
 <?php
 
-//Spades = 0-12 
-//Hearts = 13-25
-//Clubs = 26-38
-//Diamonds = 39-51
-
 $deck = array();
 $Player1 = array();
 $Player2 = array();
 $Player3 = array();
 $Player4 = array();
 $playerHand = array($Player1, $Player2, $Player3, $Player4);
+$suitArray = array("spades", "hearts", "clubs", "diamonds");
 $playerSum = array(0, 0, 0, 0);
 
 for($i=0; $i < 52; $i++){
     $deck[] = $i;
 }
 
-array_pop($deck);
 shuffle($deck);
 
 for($i=0; $i < 4; $i++){
     while($playerSum[$i] <= 36){
         $val = array_pop($deck);
-        $playerSum[$i] += $val%13;
+        $playerSum[$i] += ($val%13)+1;
         array_push($playerHand[$i], $val);
     }
 }
 
-print_r($playerSum);
-
-function displayRandomImage(){
-    global $deck;
-    $suitArray = array("clubs", "diamonds", "hearts", "spades");
-    $index = rand(0,3);
-    $randomSuit = $suitArray[$index];
-    echo  "<img src='img/cards/cards/$randomSuit/".rand(1,13).".png'/>";
+for($i=0; $i<5; $i++){
+    $hand = array_pop($playerHand);
+    //echo "<br/>";
+    for($j=0;$j<count($hand); $j++){
+        $suitIndex = floor($hand[$j]/13);
+        $suit = $suitArray[$suitIndex];
+        $num = ($hand[$j]%13)+1;
+        echo  "<img src='../img/cards/cards/$suit/$num.png'/>";
+    }
+    echo "<br/>";
 }
 
 ?>
@@ -47,10 +44,8 @@ function displayRandomImage(){
         <title> Arrays Review </title>
     </head>
     <body>
-
-      <img src="img/cards/cards/clubs/<?=rand(1,13)?>.png" />
       
-      <?=displayRandomImage()?>
+      
       
     </body>
 </html>
